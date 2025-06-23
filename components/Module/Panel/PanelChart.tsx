f9423025fd5ce38131f9da0c207a1d1386dd59cc
Mahdi <mahdibagherichanel@gmail.com>
2025-06-23 14:44:48 +0330

    add chart

--- components/Module/Panel/PanelChart.tsx
+++ components/Module/Panel/PanelChart.tsx
@@ -3,16 +3,17 @@
 import React from "react";
 import LineChart from "./LineChart";
 import PieChart from "./PieChart";
-
+import AnotherChart from "./AnotherChart";
 export default function PanelChart() {
   return (
     <>
-      <h2 className="text-2xl font-bold mb-6">پنل مدیریت</h2>
+      <h2 className="text-2xl font-bold">پنل مدیریت</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="bg-white p-6 rounded-lg shadow">
+        <div className="bg-white rounded-lg shadow space-y-6 p-4">
           <LineChart />
+          <AnotherChart />
         </div>
-        <div className="bg-white p-6 rounded-lg shadow">
+        <div className="bg-white rounded-lg shadow p-4">
           <PieChart />
         </div>
       </div>
